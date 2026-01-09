cb103d570e2bd3bb93bb60469610af9f2b0bf955
ashxvegeta <ashutoshroy453@gmail.com>
2026-01-09 09:14:08 +0530

    Addded memory to chatbot

--- rag/rag_engine.py
+++ rag/rag_engine.py
@@ -2,7 +2,7 @@ from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
-from langchain_core.messages import SystemMessage, HumanMessage
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 import os
 
 class RAGEngine:
@@ -12,6 +12,7 @@ class RAGEngine:
         self.pdf_path = pdf_path
         self.llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
         self.vectorstore = None
+        self.chat_history = []
         self._load_and_index_pdf()
 
     def _load_and_index_pdf(self):
@@ -30,7 +31,7 @@ class RAGEngine:
         self.vectorstore = FAISS.from_documents(chunks, embeddings)
 
     def ask(self, query: str) -> str:
-        # retrieve relevant documents
+        #  Retrieve relevant docs
         relevant_docs = self.vectorstore.similarity_search(query, k=3)
 
         if not relevant_docs:
@@ -38,13 +39,30 @@ class RAGEngine:
 
         context = "\n\n".join([doc.page_content for doc in relevant_docs])
 
-        # prompt LLM with context
-        message = [
-            SystemMessage(content="Answer ONLY using the provided context. If not found, say 'The question you are asking is not in document.."),
-            HumanMessage(content=f"Context:\n{context}\n\nQuestion: {query}")
+        #  Build message list
+        messages = [
+            SystemMessage(
+                content="Answer ONLY using the provided context. "
+                        "If not found, say 'The question you are asking is not in document.'"
+            )
         ]
-        
-        response = self.llm.invoke(message)
-        return response.content.strip()
 
-    
+        #  Add previous memory
+        messages.extend(self.chat_history)
+
+        # Add current question WITH context
+        messages.append(
+            HumanMessage(content=f"Context:\n{context}\n\nQuestion: {query}")
+        )
+
+        #  Ask LLM
+        response = self.llm.invoke(messages)
+        answer = response.content.strip()
+
+        # Save conversation to memory
+        self.chat_history.append(HumanMessage(content=query))
+        self.chat_history.append(AIMessage(content=answer))
+
+        return answer
+
+        
